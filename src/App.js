991343089b12c300c8cc8277ca323169cff8c640
shaikhamaan <amaan.shaikh@walchandsangli.ac.in>
2021-11-23 23:32:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,83 @@
-import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import './scss/style.scss';
-
-const loading = (
-  <div className="pt-3 text-center">
-    <div className="sk-spinner sk-spinner-pulse"></div>
-  </div>
-)
+import React, { Component } from "react";
+import {
+  HashRouter,
+  Route,
+  Switch,
+  Redirect,
+  useHistory,
+} from "react-router-dom";
+import "./scss/style.scss";
+import authRoutes from "./authRoutes";
+import { useDispatch } from "react-redux";
+import localStorageConstants from "src/constants/localstorageConstants";
+import { SET_LOADER, SET_LOGIN_STATUS } from "src/redux/actions";
+import { useSelector } from "react-redux";
+import { CFade } from "@coreui/react";
+import Loader from "./components/loader";
+const Loading = () => {
+  return (
+    <div className="pt-3 text-center">
+      <div className="sk-spinner sk-spinner-pulse"></div>
+    </div>
+  );
+};
 
 // Containers
-const TheLayout = React.lazy(() => import('./containers/TheLayout'));
+const TheLayout = React.lazy(() => import("./containers/TheLayout"));
 
 // Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'));
-const Register = React.lazy(() => import('./views/pages/register/Register'));
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
+const Login = React.lazy(() => import("./views/pages/login/Login"));
+const Register = React.lazy(() => import("./views/pages/register/Register"));
+const Page404 = React.lazy(() => import("./views/pages/page404/Page404"));
+const Page500 = React.lazy(() => import("./views/pages/page500/Page500"));
 
-class App extends Component {
-
-  render() {
-    return (
-      <HashRouter>
-          <React.Suspense fallback={loading}>
-            <Switch>
-              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
-              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
-              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
-              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
-            </Switch>
-          </React.Suspense>
-      </HashRouter>
-    );
+function App() {
+  const dispatch = useDispatch();
+  const userId = localStorage.getItem(localStorageConstants.userId);
+  const loginStatus = useSelector((state) => state?.commonReducer?.loginStatus);
+  const loading = useSelector((state) => state.commonReducer.loader);
+  const history = useHistory();
+  console.log(loginStatus, loading, "gyta");
+  if (userId) {
+    dispatch({ type: SET_LOGIN_STATUS, payload: true });
+    history.push("/dashboard");
+  } else {
+    history.push("/login");
   }
+
+  return (
+    <>
+      <Loader loading={loading} />
+      <Switch>
+        {authRoutes.map((route, idx) => {
+          return (
+            route.component && (
+              <Route
+                key={idx}
+                path={route.path}
+                exact={route.exact}
+                name={route.name}
+                render={(props) => (
+                  <CFade>
+                    <route.component {...props} />
+                  </CFade>
+                )}
+              />
+            )
+          );
+        })}
+        {loginStatus ? (
+          <Route
+            //exact={true}
+            path="/"
+            name="Home"
+            render={(props) => <TheLayout {...props} />}
+          />
+        ) : null}
+      </Switch>
+    </>
+  );
 }
 
 export default App;
+export { Loading };
--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, Redirect, useHistory } from "react-router-dom";
 import {
   CButton,
   CCard,
@@ -12,13 +12,100 @@ import {
   CInputGroup,
   CInputGroupPrepend,
   CInputGroupText,
-  CRow
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-
+  CRow,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import { useDispatch } from "react-redux";
+import localStorageConstants from "src/constants/localstorageConstants";
+import { SET_LOADER, SET_LOGIN_STATUS } from "src/redux/actions";
+import PasswordInput from "src/components/formFields/passwordInput";
+import backgroundImage from "src/constants/images/Background.png";
+import BrandLogo from "src/assets/images/logo-brand.png";
+import axios from "axios";
 const Login = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const [userId, setUserId] = useState(
+    localStorage.getItem(localStorageConstants.userId)
+  );
+
+  const onLoginPress = async () => {
+    dispatch({ type: SET_LOADER, payload: true });
+    const response = await axios.post("http://localhost:5000/authuser", {
+      username: id,
+      password: password,
+    });
+    if (Response.status === "success") {
+      localStorage.setItem(localStorageConstants.userId, id);
+      localStorage.setItem("role", response?.role);
+      dispatch({ type: SET_LOGIN_STATUS, payload: true });
+      history.push("/dashboard");
+    } else {
+      setErrors({ message: "Login Failed" });
+    }
+    dispatch({ type: SET_LOADER, payload: false });
+    // login(
+    //   {
+    //     email: id,
+    //     password: password,
+    //     device_type: "Web",
+    //   },
+    //   (data) => {
+    //     if (data?.status === "success") {
+    //
+    //       localStorage.setItem(
+    //         localStorageConstants.sessionId,
+    //         data?.data?.session_id
+    //       );
+    //       dispatch({
+    //         type: SET_USER_DATA,
+    //         payload: data?.data?.user_details,
+    //       });
+    //       setUserId(data?.data?.user_details?.user_id);
+    //       getMetaData(
+    //         `?user_id=${data?.data?.user_details?._id}`,
+    //         (data) => {
+    //           if (data?.status === "success" && data?.data) {
+    //             dispatch({
+    //               type: SET_META_DATA,
+    //               payload: data?.data,
+    //             });
+    //           }
+    //         },
+    //         () => {
+    //           dispatch({ type: SET_LOADER, payload: false });
+    //           setErrors({ message: "Login Failed" });
+    //         }
+    //       );
+    //       if (data?.data?.user_details?.user_type === "hr") {
+    //         history.push("/employees/list");
+    //       } else {
+    //         history.push("/dashboard");
+    //       }
+    //       dispatch({ type: SET_LOADER, payload: false });
+    //     } else {
+    //       setErrors({ message: data?.message });
+    //       dispatch({ type: SET_LOADER, payload: false });
+    //     }
+    //   },
+    //   () => {
+    //     dispatch({ type: SET_LOADER, payload: false });
+    //   }
+    // );
+  };
   return (
-    <div className="c-app c-default-layout flex-row align-items-center">
+    <div
+      className="c-app c-default-layout flex-row align-items-center hide-text-selection"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <CContainer>
         <CRow className="justify-content-center">
           <CCol md="8">
@@ -34,36 +121,75 @@ const Login = () => {
                           <CIcon name="cil-user" />
                         </CInputGroupText>
                       </CInputGroupPrepend>
-                      <CInput type="text" placeholder="Username" autoComplete="username" />
+                      <CInput
+                        type="text"
+                        placeholder="Username"
+                        autoComplete="username"
+                        value={id}
+                        onChange={(e) => {
+                          setId(e.target.value);
+                        }}
+                      />
                     </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupPrepend>
-                        <CInputGroupText>
-                          <CIcon name="cil-lock-locked" />
-                        </CInputGroupText>
-                      </CInputGroupPrepend>
-                      <CInput type="password" placeholder="Password" autoComplete="current-password" />
-                    </CInputGroup>
+                    <div className="mb-4">
+                      <PasswordInput value={password} setValue={setPassword} />
+                    </div>
+
+                    {errors?.message ? (
+                      <CRow className="mb-4">
+                        <CCol xs="12">
+                          <span style={{ color: "red", fontWeight: 500 }}>
+                            {errors?.message}
+                          </span>
+                        </CCol>
+                      </CRow>
+                    ) : null}
                     <CRow>
-                      <CCol xs="6">
-                        <CButton color="primary" className="px-4">Login</CButton>
-                      </CCol>
-                      <CCol xs="6" className="text-right">
-                        <CButton color="link" className="px-0">Forgot password?</CButton>
+                      <CCol xs="4">
+                        <CButton
+                          onClick={onLoginPress}
+                          color="primary"
+                          className="px-4"
+                        >
+                          Login
+                        </CButton>
                       </CCol>
                     </CRow>
                   </CForm>
                 </CCardBody>
               </CCard>
-              <CCard className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
+              <CCard
+                className="text-white bg-primary py-5 d-md-down-none"
+                style={{ width: "44%" }}
+              >
                 <CCardBody className="text-center">
                   <div>
-                    <h2>Sign up</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                      labore et dolore magna aliqua.</p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>Register Now!</CButton>
-                    </Link>
+                    <div>
+                      <img
+                        src={BrandLogo}
+                        alt="sd"
+                        style={{
+                          height: 100,
+                          width: "auto",
+
+                          marginBottom: 16,
+                        }}
+                      />
+                    </div>
+                    <h2>FreshExpress </h2>
+                    <p>
+                      FreshExpress Payroll and attendance management system!
+                    </p>
+                    {/* <Link to="/">
+                      <CButton
+                        color="primary"
+                        className="mt-3"
+                        active
+                        tabIndex={-1}
+                      >
+                        Visit Now!
+                      </CButton>
+                    </Link> */}
                   </div>
                 </CCardBody>
               </CCard>
@@ -72,7 +198,7 @@ const Login = () => {
         </CRow>
       </CContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
